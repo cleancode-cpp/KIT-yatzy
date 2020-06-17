eb203b195db1492a048ed632012272ec367476c0
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-06-17 14:03:41 +0200

    step2

--- YatzyScore.h
+++ YatzyScore.h
@@ -5,9 +5,20 @@
 using Score = int;
 using EyeCount = int;
 using Dices = std::array<EyeCount, 5>;
+using Count = int;
+
+using Histogram = std::array<Count, 6>;
+
+auto buildHistogram(Dices dices) -> Histogram {
+  auto h = Histogram{};
+  for (auto eyeCount : dices) {
+    h[eyeCount - 1] += 1;
+  }
+  return h;
+}
 
 struct YatzyScore {
-  static auto singles(EyeCount eyeCount, Dices dices) -> Score {
+  static auto byEyeCount(EyeCount eyeCount, Dices dices) -> Score {
     // auto score = Score{};
     // for (auto diceEyeCount : dices) {
     //   if (diceEyeCount == eyeCount)
@@ -18,10 +29,28 @@ struct YatzyScore {
     return eyeCount * std::count(begin(dices), end(dices), eyeCount);
   }
 
-  static auto ones(Dices dices) -> Score { return singles(1, dices); }
-  static auto twos(Dices dices) -> Score { return singles(2, dices); }
-  static auto threes(Dices dices) -> Score { return singles(3, dices); }
-  static auto fours(Dices dices) -> Score { return singles(4, dices); }
-  static auto fives(Dices dices) -> Score { return singles(5, dices); }
-  static auto sixes(Dices dices) -> Score { return singles(6, dices); }
+  static auto onlyOnes(Dices dices) -> Score { return byEyeCount(1, dices); }
+  static auto onlyTwos(Dices dices) -> Score { return byEyeCount(2, dices); }
+  static auto onlyThrees(Dices dices) -> Score { return byEyeCount(3, dices); }
+  static auto onlyFours(Dices dices) -> Score { return byEyeCount(4, dices); }
+  static auto onlyFives(Dices dices) -> Score { return byEyeCount(5, dices); }
+  static auto onlySixes(Dices dices) -> Score { return byEyeCount(6, dices); }
+
+  static auto bestPair(Dices dices) -> Score {
+    auto h = buildHistogram(dices);
+    for (auto i = 0; i < h.size(); i++) {
+      if (h[h.size() - i - 1] >= 2) {
+        return (h.size() - i) * 2;
+      }
+    }
+    return Score{};
+
+    //     0 1 2 3 4 5
+    //     ^begin      ^end
+    //   ^rend       ^rbegin
+    // auto it = std::find_if(h.rbegin(), h.rend(), [](Count count) {
+    //   return count >= 2; //
+    // });
+    // return (h.rend() - it) * 2;
+  }
 };
--- YatzyScoreTests.cc
+++ YatzyScoreTests.cc
@@ -3,5 +3,13 @@
 #include "YatzyScore.h"
 
 TEST(YatzyScore, ones) {
-  EXPECT_EQ(0, YatzyScore::ones({2, 3, 4, 5, 6})); //
+  EXPECT_EQ(3, YatzyScore::onlyOnes({1, 3, 1, 5, 1})); //
+}
+
+TEST(YatzyScore, pair_example) {
+  EXPECT_EQ(8, YatzyScore::bestPair({2, 2, 4, 4, 6})); //
+}
+
+TEST(YatzyScore, pair_nopair) {
+  EXPECT_EQ(0, YatzyScore::bestPair({2, 3, 4, 5, 6})); //
 }
